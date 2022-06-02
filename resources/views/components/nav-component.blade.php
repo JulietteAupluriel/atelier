f8423d466681b21f010edd4ab41a6d24cd788279
Juliette <juliette@aupluriel.be>
2022-06-02 17:40:45 +0200

    navcomponent

--- resources/views/components/nav-component.blade.php
+++ resources/views/components/nav-component.blade.php
@@ -1,7 +1,3 @@
 <div>
-    <!-- The only way to do great work is to love what you do. - Steve Jobs -->
-  <!--  @foreach( $sections as $section)
-       <li><a href="{{ route('sections.show', $section->id)}}">{{ $section->title }}</a></li>
-       @endforeach-->
-       
+ 
 </div>
